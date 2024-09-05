122415eb7e70d477f2fee84fb57b34208fb71624
atharva <atharvagholap24@gmail.com>
2024-09-05 23:42:21 +0530

    after backend is set on ec2

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const serverUrl = 'https://wise-social-media-backend.vercel.app' //'http://localhost:3001' 
+export const serverUrl = `http://35.154.189.136:${process.env.PORT || 3001}` //'http://localhost:3001' 
 //'https://wise-social-media-backend.vercel.app'
